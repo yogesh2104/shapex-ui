bd479bc454e1d339bb20af956b55df37999c46ac
Yogesh Singh <singhyogesh2104@gmail.com>
2024-12-23 16:54:26 +0530

    fix:remove un-used variable

--- registry/default/blocks/login-02/page.tsx
+++ registry/default/blocks/login-02/page.tsx
@@ -1,9 +1,5 @@
 import { LoginFormTwo } from "@/registry/default/blocks/login-02/components/login-form-two";
 
-export const iframeHeight = "600px";
-export const containerClassName =
-  "[&>div]:w-full [&>div]:max-w-md flex items-center justify-center min-h-svh";
-
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
