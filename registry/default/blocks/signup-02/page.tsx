93e94d00d468ba2556d9af569074efdce9fd51a9
Yogesh Singh <singhyogesh2104@gmail.com>
2024-12-24 10:25:40 +0530

    css fix

--- registry/default/blocks/signup-02/page.tsx
+++ registry/default/blocks/signup-02/page.tsx
@@ -1,5 +1,9 @@
 import SignUpTwo from "@/registry/default/blocks/signup-02/components/signup-two";
 
 export default function Page() {
-  return <SignUpTwo />;
+  return (
+  <div className="p-2 ">
+    <SignUpTwo />
+  </div>
+  );
 }
