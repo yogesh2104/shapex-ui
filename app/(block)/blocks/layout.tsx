156e81d47d8e3c96fe65b76f868febed6d1a8f37
Yogesh Singh <singhyogesh2104@gmail.com>
2025-01-09 14:02:58 +0530

    fix: add suspence in useseachparam

--- app/(block)/blocks/layout.tsx
+++ app/(block)/blocks/layout.tsx
@@ -1,6 +1,7 @@
 import { BlockSideBar } from "@/components/block-sidebar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { docsConfig } from "@/config/docs";
+import { Suspense } from "react";
 
 
 interface DocsLayoutProps {
@@ -11,6 +12,7 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
 
     <>
+    <Suspense fallback={null}>
       <div className="container flex-1 items-start md:grid md:grid-cols-[200px_minmax(0,1fr)] md:gap-2 lg:grid-cols-[210px_minmax(0,1fr)]">
         <aside className="fixed border-r pr-1 top-14 z-30 -ml-2 hidden h-[calc(100vh-4.1rem)] w-full shrink-0 md:sticky md:block">
           <ScrollArea className="h-full pr-3">
@@ -19,6 +21,7 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
         </aside>
         <div className="flex-1">{children}</div>
       </div>
+    </Suspense>
     </>
   );
 }
